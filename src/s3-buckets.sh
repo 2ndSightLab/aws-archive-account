7a3fa82b1c1c752559dc88925d3be6b96a9aa558
EC2 Default User <ec2-user@ip-10-20-30-46.us-east-2.compute.internal>
2025-08-21 15:55:32 -0400

    archive an S3 bucket

--- src/s3-buckets.sh
+++ src/s3-buckets.sh
@@ -12,7 +12,8 @@ if one exists to decrypt the contents of the bucket.
 
 END_TEXT
 
-read -p "Would you like to see the required policies to apply to IAM roles and bucket? (y)" read v
+read -p "Would you like to see the required policies to apply to IAM roles and bucket? (y): " v
+
 if [ "$v" == "y" ]; then
 
 cat <<'END_TEXT' 
@@ -59,7 +60,9 @@ Add bucket policy for each bucket in the from account:
     ]
 }
 
-If the bucket is encrypted make sure the key policy grants access to the archive admin role:
+If the bucket is encrypted make sure the key policy grants access to the archive admin role.
+The permissions need to be added to the keys on buckets in both accounts. In addition,
+the role executing the sync command needs KMS permissions in the IAM role policy.
 
 {
     "Version": "2012-10-17",
@@ -99,11 +102,64 @@ for bucket_name in $(aws s3api list-buckets --query "Buckets[].Name" --output te
 done
 echo ""
 
-p_name="all"
+from_bucket="all"
 
-while [[ -n $p_name ]]; do
-   read -p "Enter the bucket name you want to archive or all. Enter to continue: " p_name
-   if [[ -n $p_name ]]; then
-     echo "TODO: Archive S3 buckets"
+while [[ -n $from_bucket ]]; do
+   read -p "Enter the bucket name you want to archive or all. Enter to continue: " from_bucket
+   if [[ -n $from_bucket ]]; then
+     if [ "$from_bucket" == "all" ]; then 
+       echo "TODO: Archiving all buckets not complete"
+     else
+       read -p "Enter the bucket to which you want to copy the files in the archive_to account:" to_bucket
+
+       if aws s3api list-buckets --profile "${archive_to}" --region "${region}" \
+         --query "Buckets[?Name=='${to_bucket}'].Name" --output text | grep -q "${to_bucket}"; then
+
+         echo "Bucket: $to_bucket exists"
+
+       else
+        read -p "Bucket: $to_bucket does not exist in the destination account. Do you want to create it?"
+
+        aws s3api create-bucket \
+          --bucket "${to_bucket}" \
+          --region "${region}" \
+          --profile "${archive_to}" \
+          --create-bucket-configuration LocationConstraint="${region}"
+      fi
+     
+      key_id=$(aws s3api get-bucket-encryption \
+      --bucket "${to_bucket}" \
+      --profile "${archive_to}" \
+      --region "${region}")
+
+      #the jq filter has to be on one line apparently or Gemini cannot tell me how to break it up without causing an error.
+      key_id=$(echo "${key_id_raw_output}" | jq -r '.ServerSideEncryptionConfiguration.Rules[] | select(.ApplyServerSideEncryptionByDefault.SSEAlgorithm == "aws:kms") | .ApplyServerSideEncryptionByDefault.KMSMasterKeyID')
+
+     echo "key_id: $key_id"
+
+     if [[ -z "${key_id}" ]]; then
+       read -p "KMS key ID not found. Enter the ARN or ID of the KMS Key to use for encryption: " key_id
+       aws s3api put-bucket-encryption \
+        --bucket "${to_bucket}" \
+        --profile "${archive_to}" \
+        --region "${region}" \
+        --server-side-encryption-configuration '{
+        "Rules": [
+        {
+          "ApplyServerSideEncryptionByDefault": {
+            "SSEAlgorithm": "aws:kms",
+            "KMSMasterKeyID": "'"${key_id}"'"
+          }
+        }
+        ]
+        }'
+      fi
+ 
+       #we need to use the role in the to account and make sure it has access to the 
+       #bucket in the from account and the kms key used to encrypt the data
+       aws s3 sync s3://$from_bucket/ s3://$to_bucket/ \
+         --profile $archive_to \
+         --region $region
+     fi
    fi
 done
